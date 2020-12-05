518c21bf499d2077ec1d0d1eb21cd6741b7f1193
mfd <minigunneryt@gmail.com>
2020-12-05 23:24:20 +0000

    The current default headers, subject to change

---
+++ src/modules/constants/headers.ts
@@ -0,0 +1,8 @@
+// REMOVE Drop p3p for the time being.
+export default {
+	'cache-control': 'no-cache',
+	apiVersion: '12',
+	pragma: 'no-cache',
+	expires: '-1',
+	// p3p: 'CP="CAO DSP COR CURa ADMa DEVa OUR IND PHY ONL UNI COM NAV INT DEM PRE"',
+};
