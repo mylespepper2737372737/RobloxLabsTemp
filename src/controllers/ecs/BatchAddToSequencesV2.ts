d32b04441f08f2da6a5f84413fb22ca674d600e4
mfd <minigunneryt@gmail.com>
2020-12-05 23:14:00 +0000

    https://ephemeralcounters.api.sitetest1.mfdlabs.com/v1.0/SequenceStatistics/BatchAddToSequencesV2

---
+++ src/controllers/ecs/BatchAddToSequencesV2.ts
@@ -0,0 +1,7 @@
+export default {
+	dir: '/v1.0/SequenceStatistics/BatchAddToSequencesV2',
+	method: 'all',
+	func: (_req: any, res: { status: (arg0: number) => { (): any; new (): any; send: { (arg0: string): void; new (): any } } }): void => {
+		res.status(200).send('k');
+	},
+};
