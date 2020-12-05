8659f511d832707363203a3aa425b32cd53e2f39
mfd <minigunneryt@gmail.com>
2020-12-05 23:25:06 +0000

    Currently used DNSs, subject to change

---
+++ src/modules/constants/urls.ts
@@ -0,0 +1,10 @@
+export default {
+	www: 'www.sitetest1.mfdlabs.com',
+	api: 'api.sitetest1.mfdlabs.com',
+	staticcdn: 'static.sitetest1.mfdlabs.com',
+	js: 'js.sitetest1.mfdlabs.com',
+	css: 'css.sitetest1.mfdlabs.com',
+	images: 'images.sitetest1.mfdlabs.com',
+	setup: 'setup.sitetest1.mfdlabs.com',
+	ephemeralcounters: 'ephemeralcounters.api.sitetest1.mfdlabs.com',
+};
