e2db2879dc819f91b02f0b8172d0f7c5f428257b
mfd <minigunneryt@gmail.com>
2020-12-06 00:15:43 +0000

    Change this to throw a 503 and render ecs503.html

--- src/controllers/ecs/BatchAddToSequencesV2.ts
+++ src/controllers/ecs/BatchAddToSequencesV2.ts
@@ -1,7 +1,11 @@
+import { _dirname } from '../../modules/constants/directories';
 export default {
 	dir: '/v1.0/SequenceStatistics/BatchAddToSequencesV2',
 	method: 'all',
-	func: (_req: any, res: { status: (arg0: number) => { (): any; new (): any; send: { (arg0: string): void; new (): any } } }): void => {
-		res.status(200).send('k');
+	func: (
+		_req: any,
+		res: { status: (arg0: number) => { (): any; new (): any; sendFile: { (arg0: string): void; new (): any } } },
+	): void => {
+		res.status(503).sendFile(_dirname + '\\views\\ecs503.html');
 	},
 };
