d9541610cf6b91b4c6829d173115db809fa7dff1
mfd <minigunneryt@gmail.com>
2020-12-10 23:45:20 +0000

    Return a HttpServer tuple

--- src/modules/constants/ssl.ts
+++ src/modules/constants/ssl.ts
@@ -1,12 +1,14 @@
 import https2 from 'spdy';
 import { Express as IApplicationBuilder } from 'express-serve-static-core';
+import { Server as HttpServer } from 'http';
+import { Server as HttpsServer } from 'https';
 import dotenv from 'dotenv';
 import { _sslname, _dirname } from '../constants/directories';
 dotenv.config({ path: _dirname + '\\.env' });
 import filestream from 'fs';
-export = (app: IApplicationBuilder, name: string): void => {
+export = (app: IApplicationBuilder, name: string): [HttpServer, HttpsServer] => {
 	try {
-		https2
+		const httpsServer = https2
 			.createServer(
 				{
 					cert: filestream.readFileSync(_sslname + '\\mfdlabs.crt', 'utf-8'),
@@ -16,7 +18,8 @@ export = (app: IApplicationBuilder, name: string): void => {
 				app,
 			)
 			.listen(443, name, () => console.log(`https://${name}:443 started.`));
-		app.listen(80, name, () => console.log(`http://${name}:80 started.`));
+		const httpServer = app.listen(80, name, () => console.log(`http://${name}:80 started.`));
+		return [httpServer, httpsServer];
 	} catch (err) {
 		throw new Error(err);
 	}
