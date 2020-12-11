961810461ca773ba6589d03a324ec7e7ca395f89
mfd <minigunneryt@gmail.com>
2020-12-11 11:45:41 +0000

    Used as a test

--- src/sockets/api/uptime.ts
+++ src/sockets/api/uptime.ts
@@ -1,8 +1,17 @@
+import ws from 'ws';
+import c from 'crypto';
 export default {
 	dir: '/uptime',
-	func: (socket: { send: (arg0: boolean) => void; close: () => void }, req: { header: any }): void => {
-		console.log(req, req.header);
-		socket.send(true);
-		socket.close();
+	func: (socket: ws, req: { headers: any }): void => {
+		console.log(req, req.headers);
+		let n = 0;
+		const i = setInterval(() => {
+			socket.send(c.createHash('sha512').update(c.randomBytes(1000)).digest('hex'));
+			n += 1;
+			if (n === 10) {
+				i.unref();
+				return socket.close();
+			}
+		}, 5000);
 	},
 };
