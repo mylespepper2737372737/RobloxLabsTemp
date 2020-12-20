e398cf9048f22d48c8df6f4fa4ddebdd762a5783
mfd <minigunneryt@gmail.com>
2020-12-20 00:38:01 +0000

    Check hostname for __tid cookie

--- src/modules/middleware/init_middleware.ts
+++ src/modules/middleware/init_middleware.ts
@@ -2,15 +2,14 @@ import crypto from 'crypto';
 import headers from '../constants/headers';
 import { RequestHandler } from 'express-serve-static-core';
 export = ((
-	req: { headers: { cookie: string }; method: string; header: (arg0: string) => string },
+	req: { headers: { cookie: string }; hostname: string; method: string; header: (arg0: string) => any },
 	res: {
 		cookie: (arg0: string, arg1: string, arg2: { maxAge: number; domain: string }) => void;
 		header: (arg0: string | typeof headers, arg1?: string) => void;
-		status: (arg0: number) => void;
 	},
 	next: () => void,
 ) => {
-	if (!req.headers.cookie || !req.headers.cookie.match(/__tid/))
+	if (!req.headers.cookie || (!req.headers.cookie.match(/__tid/) && req.hostname === 'www.sitetest1.mfdlabs.com'))
 		res.cookie('__tid', crypto.createHash('sha256').update(crypto.randomBytes(1000)).digest('hex'), {
 			maxAge: 3.154e14,
 			domain: '.sitetest1.mfdlabs.com',
