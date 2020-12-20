ebbdc8d5388c4611f096d1aab889257ee5922804
mfd <minigunneryt@gmail.com>
2020-12-20 15:55:08 +0000

    Fix this for #6

--- src/modules/constants/clearCachedSessions.ts
+++ src/modules/constants/clearCachedSessions.ts
@@ -4,6 +4,7 @@ export = () => {
 	return new Promise((resolve, reject) => {
 		try {
 			fs.rmdirSync(_dirname + '\\manifest\\sessions', { recursive: true });
+			fs.mkdirSync(_dirname + '\\manifest\\sessions');
 			resolve();
 		} catch (err) {
 			reject(err);
