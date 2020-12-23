0478358b2020d50173d7eea5cf31cacae8652269
mfd <minigunneryt@gmail.com>
2020-12-23 16:32:26 +0000

    Change version from 12 to 2, we don't know why it was 12, and remove p3p along with notice

--- src/modules/constants/headers.ts
+++ src/modules/constants/headers.ts
@@ -25,11 +25,9 @@
 	***
 */
 
-// REMOVE Drop p3p for the time being.
 export default {
 	'cache-control': 'no-cache',
-	apiVersion: '12',
+	apiVersion: '2',
 	pragma: 'no-cache',
 	expires: '-1',
-	// p3p: 'CP="CAO DSP COR CURa ADMa DEVa OUR IND PHY ONL UNI COM NAV INT DEM PRE"',
 };
