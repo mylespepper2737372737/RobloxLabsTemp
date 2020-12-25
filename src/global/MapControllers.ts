c99a87d55ff9ed2766a8007ac625d5054b43e6c7
mfd <minigunneryt@gmail.com>
2020-12-25 13:30:16 +0000

    Use FastLog

--- src/global/MapControllers.ts
+++ src/global/MapControllers.ts
@@ -27,33 +27,40 @@
 
 import { Express as IApplicationBuilder, Request, Response } from 'express-serve-static-core';
 import filestream from 'fs';
-const _dirname = 'C:\\Users\\Padraig\\Git\\Mfd\\Web\\mfdlabs.com\\lib';
+import { FASTLOG3, FASTLOG6, FLog, LOGGROUP } from '../modules/Helpers/Log';
+import { _dirname } from '../modules/constants/directories';
+
 interface EndpointOpts {
 	path?: string;
 	logSetups?: boolean;
 	apiName?: string;
 }
 const MapControllers = (app?: IApplicationBuilder, opts?: EndpointOpts): Promise<void> => {
+	LOGGROUP(opts.apiName);
+
 	return new Promise((r) => {
 		let controllers: string[];
+
 		try {
 			controllers = filestream.readdirSync(
 				(opts !== undefined ? opts.path : _dirname + '\\controllers') || _dirname + '\\controllers',
 			);
 		} catch (err) {
-			return console.error(err);
+			return FASTLOG6(FLog[opts.apiName], err);
 		}
 		controllers.forEach((v) => {
 			if (!v.includes('.js.map') || !v.includes('.d.ts')) {
 				let map: {
 					default: { dir: string; func: (request: Request, Response: Response) => unknown; method: string };
 				};
+
 				try {
 					map = require(((opts !== undefined ? opts.path + '\\' : _dirname + '\\controllers\\') || _dirname + '\\controllers\\') +
 						v);
 				} catch (err) {
-					return console.error(err);
+					return FASTLOG6(FLog[opts.apiName], err);
 				}
+
 				let dir: string;
 				let func: (request: Request, Response: Response) => unknown;
 				let method: string;
@@ -64,45 +71,56 @@ const MapControllers = (app?: IApplicationBuilder, opts?: EndpointOpts): Promise
 					else return;
 					if (map.default.method) method = map.default.method.toLowerCase();
 					else return;
+
 					try {
 						if (method === 'get') {
-							if (opts.logSetups) console.log(`Mapping GET ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
+							if (opts.logSetups)
+								FASTLOG3(FLog[opts.apiName], `Mapping GET ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
 							app.get(dir, func);
 						} else if (method === 'head') {
-							if (opts.logSetups) console.log(`Mapping HEAD ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
+							if (opts.logSetups)
+								FASTLOG3(FLog[opts.apiName], `Mapping HEAD ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
 							app.head(dir, func);
 						} else if (method === 'post') {
-							if (opts.logSetups) console.log(`Mapping POST ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
+							if (opts.logSetups)
+								FASTLOG3(FLog[opts.apiName], `Mapping POST ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
 							app.post(dir, func);
 						} else if (method === 'put') {
-							if (opts.logSetups) console.log(`Mapping PUT ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
+							if (opts.logSetups)
+								FASTLOG3(FLog[opts.apiName], `Mapping PUT ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
 							app.put(dir, func);
 						} else if (method === 'delete') {
-							if (opts.logSetups) console.log(`Mapping DELETE ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
+							if (opts.logSetups)
+								FASTLOG3(FLog[opts.apiName], `Mapping DELETE ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
 							app.delete(dir, func);
 						} else if (method === 'connect') {
-							if (opts.logSetups) console.log(`Mapping CONNECT ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
+							if (opts.logSetups)
+								FASTLOG3(FLog[opts.apiName], `Mapping CONNECT ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
 							app.connect(dir, func);
 						} else if (method === 'options') {
-							if (opts.logSetups) console.log(`Mapping OPTIONS ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
+							if (opts.logSetups)
+								FASTLOG3(FLog[opts.apiName], `Mapping OPTIONS ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
 							app.options(dir, func);
 						} else if (method === 'trace') {
-							if (opts.logSetups) console.log(`Mapping TRACE ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
+							if (opts.logSetups)
+								FASTLOG3(FLog[opts.apiName], `Mapping TRACE ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
 							app.trace(dir, func);
 						} else if (method === 'patch') {
-							if (opts.logSetups) console.log(`Mapping PATCH ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
+							if (opts.logSetups)
+								FASTLOG3(FLog[opts.apiName], `Mapping PATCH ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
 							app.patch(dir, func);
 						} else if (method === 'all') {
-							if (opts.logSetups) console.log(`Mapping ALL ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
+							if (opts.logSetups)
+								FASTLOG3(FLog[opts.apiName], `Mapping ALL ${(opts.apiName ? 'https://' + opts.apiName : '') + dir}`);
 							app.all(dir, func);
 						} else {
-							throw 'Method not supported';
+							return FASTLOG6(FLog[opts.apiName], 'Error in requesting controller');
 						}
 					} catch (err) {
-						return console.error(err);
+						return FASTLOG6(FLog[opts.apiName], err);
 					}
 				} else {
-					throw new Error(`${v} had no default export.`);
+					return FASTLOG6(FLog[opts.apiName], `${v} had no default export.`);
 				}
 			}
 		});
