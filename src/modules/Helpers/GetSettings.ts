b5367df189cadc683f3fa792c298315c6b15fb86
mfd-co <minigunneryt@gmail.com>
2020-12-27 14:13:26 +0000

    Add settingsGroup and FSettings support

--- src/modules/Helpers/GetSettings.ts
+++ src/modules/Helpers/GetSettings.ts
@@ -8,27 +8,35 @@ export enum Group {
 	DFInt,
 	FString,
 	DFString,
+	FSettings,
+	All,
 }
 
-export const GetSettings = <SettingsGroup extends Group>(settingsGroup?: SettingsGroup): Record<string, unknown> | string[] | Error => {
+export const GetSettings = <SettingsType extends Group>(
+	settingsType: SettingsType,
+	settingsGroup: string = 'Web',
+): Record<string, unknown> | string[] | Error => {
 	const settings = JSON.parse(filestream.readFileSync(_dirname + '\\global\\flags.json', 'ascii'));
-	if (settingsGroup || settingsGroup === 0 || settingsGroup === Group.FFlag) {
-		switch (settingsGroup as Group) {
+	if (settingsType || settingsType === 0 || settingsType === Group.FFlag) {
+		switch (settingsType as Group) {
 			case Group.FFlag:
-				return settings['FFlag'] as Record<string, boolean>;
+				return settings[settingsGroup]['FFlag'] as Record<string, boolean>;
 			case Group.DFFlag:
-				return settings['DFFlag'] as Record<string, boolean>;
+				return settings[settingsGroup]['DFFlag'] as Record<string, boolean>;
 			case Group.FInt:
-				return settings['FInt'] as Record<string, number>;
+				return settings[settingsGroup]['FInt'] as Record<string, number>;
 			case Group.DFInt:
-				return settings['DFInt'] as Record<string, number>;
+				return settings[settingsGroup]['DFInt'] as Record<string, number>;
 			case Group.FString:
-				return settings['FString'] as Record<string, string>;
+				return settings[settingsGroup]['FString'] as Record<string, string>;
 			case Group.DFString:
-				return settings['DFString'] as Record<string, string>;
+				return settings[settingsGroup]['DFString'] as Record<string, string>;
+			case Group.FSettings:
+				return settings['FSettings'] as string[];
+			case Group.All:
+				return settings[settingsGroup] as Record<string, object>;
 			default:
-				return new Error(`Settings Group '${settingsGroup}' doesn't exist.`);
+				return new Error(`Settings Group '${settingsType}' doesn't exist.`);
 		}
 	}
-	return settings;
 };
