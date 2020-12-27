07800f90b5859a796c89bbf1398676fab0a8c516
mfd-co <minigunneryt@gmail.com>
2020-12-27 17:26:36 +0000

    Add cookie check

--- src/modules/middleware/init_middleware.ts
+++ src/modules/middleware/init_middleware.ts
@@ -29,19 +29,13 @@ import crypto from 'crypto';
 import headers from '../constants/headers';
 import { RequestHandler } from 'express-serve-static-core';
 
-export = ((
-	req: { headers: { cookie: string }; hostname: string; method: string; header: (arg0: string) => any },
-	res: {
-		cookie: (arg0: string, arg1: string, arg2: { maxAge: number; domain: string }) => void;
-		header: (arg0: string | typeof headers, arg1?: string) => void;
-	},
-	next: () => void,
-) => {
+export = ((req, res, next) => {
 	if (!req.headers.cookie || (!req.headers.cookie.match(/__tid/) && req.hostname === 'www.sitetest1.mfdlabs.com'))
 		res.cookie('__tid', crypto.createHash('sha256').update(crypto.randomBytes(1000)).digest('hex'), {
 			maxAge: 3.154e14,
 			domain: '.sitetest1.mfdlabs.com',
 		});
+
 	if (req.method !== 'GET') {
 		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 		res.header('Access-Control-Allow-Origin', req.header('Origin') || req.header('Host'));
@@ -49,5 +43,16 @@ export = ((
 		res.header('Access-Control-Allow-Credentials', 'true');
 	}
 	res.header(headers);
+	if (req.headers.cookie && !req.headers.cookie.includes('authId') && req.hostname === 'www.sitetest1.mfdlabs.com' && req.path === '/') {
+		return res.redirect('https://www.sitetest1.mfdlabs.com/Login/');
+	}
+	if (
+		req.headers.cookie &&
+		req.headers.cookie.includes('authId') &&
+		req.hostname === 'www.sitetest1.mfdlabs.com' &&
+		(req.path === '/Login' || req.path === '/Login/')
+	) {
+		return res.redirect('https://www.sitetest1.mfdlabs.com/');
+	}
 	next();
 }) as RequestHandler;
