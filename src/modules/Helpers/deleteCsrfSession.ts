b5f351c4b278771fdd6ca4cc3518bd22112f858f
mfd-co <minigunneryt@gmail.com>
2020-12-28 18:27:45 +0000

    Clears a csrf session based on name

---
+++ src/modules/Helpers/deleteCsrfSession.ts
@@ -0,0 +1,10 @@
+import filestream from 'fs';
+import { _dirname } from '../constants/directories';
+
+export = (authId: string) => {
+	try {
+		filestream.unlinkSync(_dirname + `\\manifest\\csrf\\${authId}.json`);
+	} catch (e) {
+		console.log(e);
+	}
+};
