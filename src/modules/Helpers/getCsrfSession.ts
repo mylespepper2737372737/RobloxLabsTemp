8029c722ca999b4d2c9ab2d71fcf96b3d7fc0039
mfd-co <minigunneryt@gmail.com>
2021-01-01 04:54:24 +0000

    Return broken session

--- src/modules/Helpers/getCsrfSession.ts
+++ src/modules/Helpers/getCsrfSession.ts
@@ -1,10 +1,12 @@
 import filestream from 'fs';
 import { _dirname } from '../constants/directories';
 
-export = (sessionFile: string) => {
+type csrfSessionType = { sub: string; token: string; c: number };
+
+export = (sessionFile: string): csrfSessionType => {
 	try {
 		return JSON.parse(filestream.readFileSync(_dirname + '\\manifest\\csrf\\' + sessionFile, { encoding: 'utf-8' }));
 	} catch {
-		return {};
+		return { c: 0, sub: '', token: '' };
 	}
 };
