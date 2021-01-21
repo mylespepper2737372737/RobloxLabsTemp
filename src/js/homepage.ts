2acefb84c11f6ec9046da7db8c20d5c8c996a53c
mfd-co <minigunneryt@gmail.com>
2021-01-21 09:57:36 +0000

    Change from sitetest to apex

--- src/js/homepage.ts
+++ src/js/homepage.ts
@@ -2,7 +2,7 @@
 	FileName: homepage.ts
 	Written By: Nikita Nikolaevich Petko
 	File Type: Module
-	Description: Used for https://www.sitetest1.mfdlabs.com/
+	Description: Used for https://www.mfdlabs.com/
 
 	All commits will be made on behalf of mfd-co to https://github.com/mfd-core/mfdlabs.com
 
@@ -30,13 +30,13 @@ if (document.location.protocol !== 'https:')
 const d = () => {
 	$.ajax({
 		dataType: 'json',
-		url: 'https://www.sitetest1.mfdlabs.com/Authentication/Logout.fxhx',
+		url: 'https://www.mfdlabs.com/Authentication/Logout.fxhx',
 		method: 'POST',
 		xhrFields: { withCredentials: true },
 		success: () => {
 			$('.body').css('color', 'green').text('Success!');
 			setTimeout(() => {
-				document.location.replace('https://www.sitetest1.mfdlabs.com');
+				document.location.replace('https://www.mfdlabs.com');
 			}, 500);
 		},
 	}).fail((response: JQuery.jqXHR) =>
@@ -47,7 +47,7 @@ const d = () => {
 };
 const x = () => {
 	$.ajax({
-		url: 'https://www.sitetest1.mfdlabs.com/Authentication/ClearAllSessionsAndReauthenticate.fxhx',
+		url: 'https://www.mfdlabs.com/Authentication/ClearAllSessionsAndReauthenticate.fxhx',
 		method: 'POST',
 		xhrFields: { withCredentials: true },
 		success: () => {
