e058ab3218fc5687f49728d8c1115ce47347872a
mfd-co <tfs@mfdlabs.com>
2021-02-07 19:30:19 +0000

    Get info on a specific universeId.

--- Roblox.Api/Roblox.Controllers/Roblox.Api/universes/get-info.ts
+++ Roblox.Api/Roblox.Controllers/Roblox.Api/universes/get-info.ts
@@ -1,4 +1,30 @@
-// import r from 'request';
+/*
+	FileName: get-info.ts
+	Written By: Nikita Nikolaevich Petko
+	File Type: Module
+	Description: http://api.sitetest4.robloxlabs.com/universes/get-info, Get info on a specific universeId.
+
+	All commits will be made on behalf of mfd-co to http://github.com/mfd-core/sitetest4.robloxlabs.com
+
+	***
+
+	Copyright 2015-2020 MFD
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+
+	***
+*/
+
 import { FASTLOG1 } from '../../../Roblox.Helpers/Roblox.Helpers/Roblox.Util/Roblox.Util.FastLog';
 
 export default {
