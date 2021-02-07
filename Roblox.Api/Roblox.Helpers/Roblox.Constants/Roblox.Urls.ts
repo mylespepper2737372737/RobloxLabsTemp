fc2f2f63fcc20736c860145123f73458f9745063
mfd-co <tfs@mfdlabs.com>
2021-02-07 21:25:57 +0000

    Define all urls here if you want to keep track of what some symbols actually refer to.

---
+++ Roblox.Api/Roblox.Helpers/Roblox.Constants/Roblox.Urls.ts
@@ -0,0 +1,45 @@
+/*
+	FileName: urls.ts
+	Written By: Aleksej Brovim Pushnak,
+	File Type: Module
+	Description: Add global URLS that are being used or will be used to this.
+
+	All commits will be made on behalf of mfd-co to http://github.com/mfd-core/sitetest4.robloxlabs.com
+
+	***
+
+	Copyright 2015-2020 MFD
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+
+	***
+*/
+
+// Define all Urls to use here, even if they aren't being used ¯\_(ツ)_/¯
+export default {
+	ROBLOX_APEX: 'sitetest4.robloxlabs.com',
+	ROBLOX_WWW: 'www.sitetest4.robloxlabs.com',
+	ROBLOX_API: 'api.sitetest4.robloxlabs.com',
+	ROBLOX_STATIC_CDN: 'static.sitetest4.robloxlabs.com',
+	ROBLOX_JS: 'js.sitetest4.robloxlabs.com',
+	ROBLOX_CSS: 'css.sitetest4.robloxlabs.com',
+	ROBLOX_IMAGES: 'images.sitetest4.robloxlabs.com',
+	ROBLOX_SETUP_CDN: 'setup.sitetest4.robloxlabs.com',
+	ROBLOX_EPHEMERAL_COUNTERS_API: 'ephemeralcounters.api.sitetest4.robloxlabs.com',
+	ROBLOX_EPHEMERAL_COUNTERS_VERSION_2: 'ecsv2.sitetest4.robloxlabs.com',
+	DEPRECATED_ROBLOX_TEMPORARY_IMAGES: 'ti.sitetest4.robloxlabs.com',
+	ROBLOX_VERSION_COMPATIBILITY_API: 'versioncompatibility.api.sitetest4.robloxlabs.com',
+	ROBLOX_CLIENT_SETTINGS_API: 'clientsettings.api.sitetest4.robloxlabs.com',
+	ROBLOX_ASSET_GAME: 'assetgame.sitetest4.robloxlabs.com',
+	ROBLOX_GAME_PERSISTENCE: 'gamepersistence.sitetest4.robloxlabs.com',
+};
