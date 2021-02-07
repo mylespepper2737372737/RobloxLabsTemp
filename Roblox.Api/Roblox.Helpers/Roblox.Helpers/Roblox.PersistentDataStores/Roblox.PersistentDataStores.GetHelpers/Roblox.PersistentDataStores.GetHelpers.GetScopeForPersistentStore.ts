0098e1a29943501e7756ecce77337b4b67d539a8
mfd-co <tfs@mfdlabs.com>
2021-02-07 21:31:36 +0000

    Helpers for getting specific things a RobloxPublicDB

---
+++ Roblox.Api/Roblox.Helpers/Roblox.Helpers/Roblox.PersistentDataStores/Roblox.PersistentDataStores.GetHelpers/Roblox.PersistentDataStores.GetHelpers.GetScopeForPersistentStore.ts
@@ -0,0 +1,62 @@
+import { _dirname } from '../../../Roblox.Constants/Roblox.Directories';
+import { WriteUniverse } from '../Roblox.PersistentDataStores.SetHelpers/Roblox.PersistentDataStores.SetHelpers.WriteUniverse';
+import { GetPersistentStoreForUniverse } from './Roblox.PersistentDataStores.GetHelpers.GetPersistentStoreForUniverse';
+import filestream from 'fs';
+
+type VersionType = {
+	version: number;
+	date: string;
+};
+
+type KeyType = {
+	keyName: string;
+	scope: string;
+	store: string;
+	universe: number;
+	version: number;
+	root: string;
+	created: string;
+	lastUpdated: string;
+	value: {
+		raw: unknown;
+		type: string;
+	};
+	versions?: VersionType[];
+};
+
+type ScopeType = {
+	scopeName: string;
+	store: string;
+	universe: number;
+	keysCount: number;
+	root: string;
+	created: string;
+	lastUpdated: string;
+	keys?: KeyType[];
+};
+
+export const GetScopeForPersistentStore = (
+	universeId: number,
+	storeName: string,
+	scopeName: string,
+	isSorted: boolean = false,
+): Promise<[boolean, ScopeType | null]> => {
+	return new Promise<[boolean, ScopeType | null]>(async (resumefunction) => {
+		const dir = _dirname + '\\Roblox.Manifest\\persistence\\' + universeId;
+		if (!filestream.existsSync(dir)) {
+			WriteUniverse(universeId);
+			return resumefunction([false, null]);
+		}
+		const [success, store] = await GetPersistentStoreForUniverse(universeId, storeName.replace('sorted_', ''), scopeName, isSorted);
+		let scope = null;
+		if (success && store !== null) {
+			store.scopes.forEach((s) => {
+				if (s.scopeName === scopeName) scope = s;
+			});
+		}
+		if (store === null) {
+			return resumefunction([false, null]);
+		}
+		return resumefunction([true, scope]);
+	});
+};
