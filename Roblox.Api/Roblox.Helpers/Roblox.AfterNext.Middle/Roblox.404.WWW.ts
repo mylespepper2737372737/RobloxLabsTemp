0696fe249a070f47d892ad722be48cb04e648ca0
mfd-co <tfs@mfdlabs.com>
2021-02-07 21:20:57 +0000

    After nexr means 404, these are 404 pages for APUs, if none is present it will infinitely load or just return 0 content

---
+++ Roblox.Api/Roblox.Helpers/Roblox.AfterNext.Middle/Roblox.404.WWW.ts
@@ -0,0 +1,36 @@
+/*
+	FileName: www.ts
+	Written By: Nikita Nikolaevich Petko
+	File Type: Module
+	Description: root 404 middleware
+
+	All commits will be made on behalf of mfd-co to http://github.com/mfd-core/sitetest4.robloxlabs.com
+
+	***
+
+	Copyright 2015-2020 MFD
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+
+	***
+*/
+
+export default (req: { url: string }, res: { redirect: (arg0: string) => any }) => {
+	return res.redirect(
+		`http://www.sitetest4.robloxlabs.com/Error.ashx?code=404&message=http://www.sitetest4.robloxlabs.com${escape(req.url)
+			.split('?')
+			.shift()} NotFound&redirect=http://www.sitetest4.robloxlabs.com${escape(req.url)
+			.split('?')
+			.shift()};http://www.sitetest4.robloxlabs.com/Error.ashx`,
+	);
+};
