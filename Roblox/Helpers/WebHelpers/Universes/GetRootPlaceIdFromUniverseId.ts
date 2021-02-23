1934521303b74c118f2a81a36090a683acf4c7af
ROBLOX Team Foundation <github@roblox.com>
2021-02-23 13:33:38 +0000

    Update validation of this

--- Roblox/Helpers/WebHelpers/Universes/GetRootPlaceIdFromUniverseId.ts
+++ Roblox/Helpers/WebHelpers/Universes/GetRootPlaceIdFromUniverseId.ts
@@ -3,10 +3,10 @@ import filestream from 'fs';
 
 export const GetRootPlaceIdFromUniverseId = (universeId: number): [boolean, number | null] => {
 	const universePath = _dirname + '\\Manifest\\universes\\' + universeId;
-	if (!filestream.statSync(universePath).isDirectory()) {
+	if (!filestream.existsSync(universePath) || !filestream.statSync(universePath).isDirectory()) {
 		return [false, null];
 	}
-	const universe = JSON.parse(filestream.readFileSync(universePath + '\\PLACE.json', 'utf-8'));
+	const universe = JSON.parse(filestream.readFileSync(universePath + '\\UNIVERSE.json', 'utf-8'));
 	if (universe instanceof Object && universe['rootPlaceId'] !== undefined) {
 		return [true, universe['rootPlaceId']];
 	}
