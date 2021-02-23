e755a12b7b6803786c0935c1c7275aae8394aaf7
ROBLOX Team Foundation <github@roblox.com>
2021-02-23 13:31:43 +0000

    Update this

--- Roblox/Controllers/Roblox.GamePersistence/v2/persistence/_P-universeId/datastores.ts
+++ Roblox/Controllers/Roblox.GamePersistence/v2/persistence/_P-universeId/datastores.ts
@@ -24,184 +24,213 @@
 
 	***
 */
-
-import { FASTLOG1, FASTLOG4, FASTLOG6 } from '../../../../../Helpers/WebHelpers/Roblox.Util/Roblox.Util.FastLog';
-import { CheckIfPlaceExists } from '../../../../../Helpers/WebHelpers/Universes/CheckIfPlaceExists';
-import { GetPlaceFromId } from '../../../../../Helpers/WebHelpers/Universes/GetPlaceFromId';
-import { GetPersistentStoresForUniverse } from '../../../../../Helpers/WebHelpers/PersistentDataStores/GetHelpers/GetPersistentStoresForUniverse';
-
 import { Request, Response } from 'express-serve-static-core';
-import { ClientSettings, Group } from '../../../../../Helpers/WebHelpers/Roblox.Util/Roblox.Util.ClientSettings';
-import Crypto from 'crypto';
+import { Roblox } from '../../../../../Api';
+import { DFLog, FastLog } from '../../../../../Helpers/WebHelpers/Roblox.Util/Roblox.Util.FastLog';
+import { Pages } from '../../../../../Data/Pages/RobloxPages';
+import { GetRootPlaceIdFromUniverseId } from '../../../../../Helpers/WebHelpers/Universes/GetRootPlaceIdFromUniverseId';
+import { GetPersistentStoresForUniverse } from '../../../../../Helpers/WebHelpers/PersistentDataStores/GetHelpers/GetPersistentStoresForUniverse';
+import { IDataStoreRespose } from '../../../../../Platform/Persistence/IDataStoreRespose';
+import Base64 from 'crypto-js/enc-base64';
+import Crpto from 'crypto-js';
+
+// IList<IDataStoreRespose> Roblox.Web.GamePersistence.GamePersistenceRequestProcessor.GetDataStoresForTheUniverse(IDataStoreRequest request, Boolean requireSecureUri)
+// Request example:
+/*
+ 
+# Get all the datastores for the universe
+GET /v2/persistence/<UNIVERSEIDGOESHERE>/datastores?prefix=<PREFIXGOESHERE>&maxItemsToReturn=<PAGESIZEGOESHERE>&exclusiveStartKey=<PAGECURSORGOESHERE> HTTP/1.1
+Host: gamepersistence.sitetest4.robloxlabs.com
+Cookie: <SECURITYTOKENGOESHERE>
+Roblox-Place-Id: <PLACEIDGOESHERE>
+
+# Response
+
+HTTP/1.1 200 OK
+content-type: application/json; charset=utf-8
+
+{
+  "datastores": [
+    {
+      "name": "<DATASTORENAMEGOESHERE>",
+      "createdTime": "<ISOCREATEDTIMEGOESHERE>",
+      "updatedTime": "<ISOLASTUPDATEDTIMEGOESHERE>",
+      "versioningEnabled": <% Roblox.ClientSettings.IsFeatureEnabled("DataStoreVersioning", "WebST4") %>
+    }
+  ],
+  "lastReturnedKey": <PAGECURSORGOESHERE>
+}
+ */
+
+/*
+{
+	"dataStores": IDataStoreRespose[],
+	"lastReturnedKey": String,
+}
+*/
+
+/* 
+For the DataStore team:
+
+If the maxItemsToReturn is 0, then it will return the first 50 DataStores found on DB74
+If the prefix is null or empty, then it will return all stores without any filtering
+The page cursor will be based on the universeId in the given request, eg. beta_<LASTRETURNEDDATASTORENAMEGOESHERE>_s+<SOMEBASE64KEY>
+After 20 minutes, or server retart, the page will be purged and a new page will be supplied.
+
+*/
+
+FastLog.DYNAMIC_LOGVARIABLE('DataStoresV2', 7);
 
 export default {
 	method: 'all',
-	func: async (req: Request, res: Response) => {
-		FASTLOG1('DataStoreV2', JSON.stringify(req.query), true);
-		FASTLOG1('DataStoreV2', JSON.stringify(req.body), true);
-		FASTLOG1('DataStoreV2', JSON.stringify(req.params), true);
-		const DFInt = ClientSettings.GetSettings(Group.DFInt, 'Client');
-		const DFFlag = ClientSettings.GetSettings(Group.DFFlag, 'Client');
-		let pageSize = parseInt(<string>req.query['maxItemsToReturn']);
-		const prefix = <string>req.query['prefix'] || '';
-		if (!pageSize) pageSize = 1;
-		// let universeId = parseInt(req.params['universeId']);
-		// if (universeId === NaN) universeId = -1;
-		let placeId = parseInt(<string>req.headers['roblox-place-id']);
-		if (!placeId && placeId !== 0) placeId = -1;
+	func: async (request: Request, response: Response) => {
+		const DFFlag = Roblox.Api.Helpers.Util.ClientSettings.GetDFFlags();
+		const DFInt2 = Roblox.Api.Helpers.Util.ClientSettings.GetDFInts('Client');
+		const DFInt = Roblox.Api.Helpers.Util.ClientSettings.GetDFInts();
 
-		if (placeId === -1) {
-			if (!DFFlag['DataStoresV2AllowNegativePlacesForStudio']) {
-				FASTLOG4('DataStoreV2', `The placeId was NaN or -1`, true);
-				return res.status(403).send({
-					errors: [
-						{
-							code: 0,
-							message: 'Not allowed',
-						},
-					],
-				});
-			}
-		}
-		if (!CheckIfPlaceExists(placeId) && !DFFlag['DataStoresV2AllowNegativePlacesForStudio']) {
-			if (req.cookies['.ROBLOSECURITY']) {
-				FASTLOG4('DataStoreV2', `The placeId (${placeId}) didn't exist, and an auth token was present.`, true);
-				return res.status(403).send({
-					errors: [
-						{
-							code: 0,
-							message: 'You do not have permission to manage this place. Universe is null.', // My ass, we're not even checking universe
-						},
-					],
-				});
-			}
-			FASTLOG4('DataStoreV2', `The placeId (${placeId}) didn't exist, and no auth token was present.`, true);
-			return res.status(403).send({
+		/*Start check for request method*/
+		if (request.method !== 'GET') {
+			FastLog.FASTLOGS(
+				DFLog['DataStoreV2'],
+				'[DFLog::DataStoresV2] We got a bad request method, it was %s when GET was expected!',
+				request.method,
+			);
+			return response.status(405).send({
 				errors: [
 					{
 						code: 0,
-						message: 'Not allowed',
+						message: `The requested resource does not support http method '${request.method}'.`,
 					},
 				],
 			});
 		}
-		if (DFFlag['DataStoreV2MaxItemsToReturnShould403OnBadNumber']) {
-			if (pageSize < 1) {
-				FASTLOG4('DataStoreV2', `The pageSize was less than 1, try snap back to 1.`, true);
-				return res.status(403).send({
-					errors: [
-						{
-							code: 0,
-							message: 'Not allowed',
-						},
-					],
-				});
-			}
+		/*End check for request method*/
 
-			if (pageSize > DFInt['DataStoreMaxPageSize']) {
-				FASTLOG4('DataStoreV2', `The pageSize was greater than DataStoreMaxPageSize (${DFInt['DataStoreMaxPageSize']})`, true);
-				return res.status(403).send({
-					errors: [
-						{
-							code: 0,
-							message: 'Not allowed',
-						},
-					],
-				});
-			}
-		}
+		/*Start check for request security token*/
+		let cookie = request.headers.cookie;
+		if (cookie === undefined) cookie = '';
+		cookie = (cookie as string).split(';').find((AuthToken) => {
+			return AuthToken.startsWith(' .ROBLOSECURITY') || AuthToken.startsWith('.ROBLOSECURITY');
+		});
+		if (cookie) cookie = cookie.split('=')[1];
+		/*End check for request security token*/
 
-		if (!ClientSettings.GetPlaceIdInPlaceFilter('DataStoresV2Enabled', placeId, 'Client')) {
-			FASTLOG4('DataStoreV2', `The placeId (${placeId}) was not in FPFilterDataStoresV2Enabled`, true);
-			return res.status(403).send({
+		/*Start check for request security token validity*/
+		// This should be done on gamepersistence.api
+		/*End check for request security token validity*/
+
+		/*Start check for Universe Id*/
+		const universeId = parseInt(request.params['universeId']);
+		if (isNaN(universeId)) {
+			FastLog.FASTLOG(DFLog['DataStoresV2'], '[DFLog::DataStoresV2] We got an Null universe, god damn');
+			return response.status(400).send({
 				errors: [
 					{
-						code: 26,
-						message: 'The universe is not allowed to access the endpoint.',
-						retryable: false,
+						code: 0,
+						message: 'The request is invalid.',
 					},
 				],
 			});
 		}
+		/*End check for Universe Id*/
 
-		const [successForPlace, place] = GetPlaceFromId(placeId);
-		if (!successForPlace) {
-			FASTLOG6('DataStoreV2', `An error occured when fetching the place, no info available.`, true);
-			return res.status(500).send({
-				error: 'InternalServerError',
-			});
+		/*Start check for place ownership/permissions*/
+		/*End check for place ownership/permissions*/
+
+		/*Start check for Rollout flag and percentage*/
+		if (!DFFlag['DataStoresV2EnabledForTheWorld'] && (DFInt['DataStoreV2RolloutPercentage'] || 0) < 100) {
+			// It's not released yet. Check if the universe is valid.
+			const [success, PlaceId] = GetRootPlaceIdFromUniverseId(universeId);
+			if (!success || PlaceId === null) {
+				FastLog.FASTLOG(DFLog['DataStoresV2'], '[DFLog::DataStoresV2] We got an Null place, that means the universe did not exist');
+				return response.status(403).send({
+					errors: [
+						{
+							code: 26,
+							message: 'The universe is not allowed to access the endpoint.',
+							retryable: (DFInt2['DataStoreApiRefreshRolloutPercentage'] || 0) >= 100,
+						},
+					],
+				});
+			}
+
+			if (!Roblox.Api.Helpers.Util.ClientSettings.GetPlaceIdInPlaceFilter('DataStoresV2Enabled', PlaceId, 'Client')) {
+				FastLog.FASTLOG1(DFLog['DataStoresV2'], '[DFLog::DataStoresV2] The place %d was not in the filter, bruh', PlaceId);
+				return response.status(403).send({
+					errors: [
+						{
+							code: 26,
+							message: 'The universe is not allowed to access the endpoint.',
+							retryable: (DFInt2['DataStoreApiRefreshRolloutPercentage'] || 0) >= 100,
+						},
+					],
+				});
+			}
 		}
+		/*End check for Rollout flag and percentage*/
 
-		const responsStores = [];
-		let keyCount = 0;
-		const [success, stores] = await GetPersistentStoresForUniverse(place.universeId);
+		/*Start Query Parsing*/
+		let PageLength = <number>parseInt(<string>request.query['maxItemsToReturn']);
+		const DataStorePrefix = request.query['prefix'] || '';
+		const PageCursor = <String>request.query.PageCursor || '';
+		const exclusiveStartKey = <String>request.query['exclusiveStartKey'] || '';
+		/*End Query Parsing*/
 
-		let hasCursor = false;
-		if (success && stores !== null) {
-			const storeCount = stores.length;
-			let count = 0;
-			stores.forEach((v) => {
-				if (count === pageSize) {
-					if (count < storeCount) {
-						hasCursor = true;
+		/*Start Cursor*/
+		let Cursor = Pages.GetPageCursorByKey(`${universeId}_${PageCursor.split('+').join('+').split('=').join('_')}`);
+		if (Cursor.length === 0) {
+			Cursor = Base64.stringify(Crpto.MD5(universeId.toString()));
+			Pages.SetPageByKey(`${universeId}_${Cursor.split('+').join('+').split('=').join('_')}`, Cursor);
+		}
+		/*End Cursor*/
+
+		/*Start Variables*/
+		let DataStores: IDataStoreRespose[] = [];
+		let lastReturnedKey = '';
+		let CanPush = true;
+		let c = '';
+		/*End Variables*/
+
+		// TODO Make this happen on gamepersistence.api.sitetest4.robloxlabs.com
+		/*Start Stores*/
+		const [Succes, Stores] = await GetPersistentStoresForUniverse(universeId);
+		Stores.sort();
+		if (Succes && Stores) {
+			Stores.forEach((store, storeNumber) => {
+				if (!isNaN(<number>PageLength) && PageLength > 0 && storeNumber >= <number>PageLength) {
+					CanPush = false;
+					if (exclusiveStartKey && exclusiveStartKey.replace(' ', '+') === c) {
+						CanPush = true;
+						DataStores = [];
+						PageLength += PageLength;
 					}
+					lastReturnedKey = c;
 					return;
 				}
-				v.scopes.forEach((s) => {
-					keyCount += s.keysCount;
-				});
-				if (DFFlag['DataStoresV2GetDataStoresAdminResponse']) {
-					if (prefix.length > 0) {
-						if (v.storeName.startsWith(prefix)) {
-							responsStores.push({
-								storeName: v.storeName,
-								type: v.type,
-								universe: v.universe,
-								scopes: v.scopeCount,
-								keys: keyCount,
-								created: Math.round(new Date(v.created).getTime()),
-								lastUpdated: v.lastUpdated ? Math.round(new Date(v.lastUpdated).getTime()) : null,
+				if (DataStorePrefix !== null && DataStorePrefix.length !== 0) {
+					if (store.storeName.startsWith(<string>DataStorePrefix)) {
+						if (CanPush)
+							DataStores.push({
+								Name: store.storeName,
+								CreatedTime: store.created,
+								UpdatedTime: store.lastUpdated,
+								VersioningEnabled: DFFlag['DataStoreVersioning'] || false,
 							});
-							count++;
-						}
-					} else {
-						responsStores.push({
-							storeName: v.storeName,
-							type: v.type,
-							universe: v.universe,
-							scopes: v.scopeCount,
-							keys: keyCount,
-							created: Math.round(new Date(v.created).getTime()),
-							lastUpdated: v.lastUpdated ? Math.round(new Date(v.lastUpdated).getTime()) : null,
-						});
-						count++;
-					}
-				} else {
-					if (prefix.length > 0) {
-						if (v.storeName.startsWith(prefix)) {
-							responsStores.push({
-								storeName: v.storeName,
-								created: Math.round(new Date(v.created).getTime()),
-								lastUpdated: v.lastUpdated ? Math.round(new Date(v.lastUpdated).getTime()) : null,
-							});
-							count++;
-						}
-					} else {
-						responsStores.push({
-							storeName: v.storeName,
-							created: Math.round(new Date(v.created).getTime()),
-							lastUpdated: v.lastUpdated ? Math.round(new Date(v.lastUpdated).getTime()) : null,
-						});
-						count++;
+						c = `beta_${store.storeName}_s+${Cursor}`;
+						return;
 					}
 				}
+				if (CanPush)
+					DataStores.push({
+						Name: store.storeName,
+						CreatedTime: store.created,
+						UpdatedTime: store.lastUpdated,
+						VersioningEnabled: DFFlag['DataStoreVersioning'] || false,
+					});
+				c = `beta_${store.storeName}_s+${Cursor}`;
 			});
 		}
-		res.status(200).send({
-			data: {
-				DataStores: responsStores,
-				Cursor: hasCursor ? 'u_' + Crypto.createHash('sha1').update(Crypto.randomBytes(1000)).digest('hex') : null,
-			},
-		});
+		/*End Stores*/
+		response.send({ datastores: DataStores.sort(), lastReturnedKey: lastReturnedKey });
 	},
 };
