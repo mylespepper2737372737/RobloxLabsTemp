816d5da48a6890624f907a7b9421f9f59220c9aa
ROBLOX Team Foundation <github@roblox.com>
2021-02-27 17:33:36 +0000

    Add the version client

---
+++ Roblox/Data/Client/Version.ts
@@ -0,0 +1,10 @@
+import { DFString, DYNAMIC_FASTSTRINGVARIABLE } from '../../Helpers/WebHelpers/Roblox.Util/Roblox.Util.FastLog';
+
+// Set this to the default 0 version in case something breaks.
+DYNAMIC_FASTSTRINGVARIABLE('WebVersion', '0.001.0.000001');
+
+export namespace Version {
+	export function GetVersion() {
+		return DFString['WebVersion'];
+	}
+}
