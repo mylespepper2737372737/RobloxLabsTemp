3eb5c6c5c04a0e78021b5f51cf564eefafa5a6be
ROBLOX Team Foundation <github@roblox.com>
2021-03-03 02:39:19 +0000

    This will validate whether or not the client can access the site with their credentials, subject change

---
+++ Roblox/Util/ValidateDoesTheWorldGetToViewTheSite.ts
@@ -0,0 +1,32 @@
+import { BaseURL } from '../Data/Client/BaseUrl';
+import {
+	DFFlag,
+	DFString,
+	DYNAMIC_FASTFLAGVARIABLE,
+	DYNAMIC_FASTSTRINGVARIABLE,
+} from '../Helpers/WebHelpers/Roblox.Util/Roblox.Util.FastLog';
+import { IHttpResponse } from '../Http/ServiceClient/Http';
+
+DYNAMIC_FASTFLAGVARIABLE('DoesTheWorldGetToViewTheSite', false);
+DYNAMIC_FASTFLAGVARIABLE('CanAdminsBypassTheSystem', false);
+DYNAMIC_FASTSTRINGVARIABLE('RobloxLabsSecurityToken', 'ljWby+/HVsZXJLfRkoljWby+/HVsZXJLfRko9mPQ9mPQ==');
+
+export function ValidateDoesTheWorldGetToViewTheSite(
+	method: string,
+	returnUrl: string,
+	secToken: string,
+	response: IHttpResponse,
+	doNotRedirect = false,
+) {
+	if (method === 'OPTIONS') return true;
+	if (
+		DFFlag('CanAdminsBypassTheSystem') &&
+		(secToken === DFString('RobloxLabsSecurityToken') || secToken === encodeURIComponent(DFString('RobloxLabsSecurityToken')))
+	)
+		return true;
+	if (!DFFlag('DoesTheWorldGetToViewTheSite')) {
+		if (!doNotRedirect) response.redirect(BaseURL.GetSecureBaseURL() + '/login/maintenance/?ReturnUrl=' + returnUrl);
+		return false;
+	}
+	return true;
+}
