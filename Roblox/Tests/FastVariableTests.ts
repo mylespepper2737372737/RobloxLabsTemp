5620482c9f273f13a6a7bc5a135ef0db0a0f5ade
ROBLOX Team Foundation <github@roblox.com>
2021-03-03 02:39:45 +0000

    Add this to test the FVariable Library

---
+++ Roblox/Tests/FastVariableTests.ts
@@ -0,0 +1,8 @@
+import { DFFlag, DYNAMIC_FASTFLAG } from '../Helpers/WebHelpers/Roblox.Util/Roblox.Util.FastLog';
+
+DYNAMIC_FASTFLAG('e');
+console.log(DFFlag('e'));
+console.log(DFFlag('e'));
+console.log(DFFlag('e'));
+console.log(DFFlag('e'));
+console.log(DFFlag('e'));
