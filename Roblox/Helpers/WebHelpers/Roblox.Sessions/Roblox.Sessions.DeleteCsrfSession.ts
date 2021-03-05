5b9fb66403a0a6696ef453b98aed0178f9db820c
ROBLOX Team Foundation <github@roblox.com>
2021-03-05 14:13:37 +0000

    Fix error logger

--- Roblox/Helpers/WebHelpers/Roblox.Sessions/Roblox.Sessions.DeleteCsrfSession.ts
+++ Roblox/Helpers/WebHelpers/Roblox.Sessions/Roblox.Sessions.DeleteCsrfSession.ts
@@ -4,7 +4,5 @@ import { _dirname } from '../../Constants/Directories';
 export const DeleteCsrfSession = (AuthToken: string) => {
 	try {
 		filestream.unlinkSync(_dirname + `\\Manifest\\csrf\\${AuthToken}.json`);
-	} catch (e) {
-		console.log(e);
-	}
+	} catch (e) {}
 };
