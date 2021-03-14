1862a5363e5c733fc9238cdcce72c0230b48568b
Nikita Petko <79342039+rbx-nsg@users.noreply.github.com>
2021-03-14 23:16:28 +0000

    Remove profanity from privatemessages

--- Roblox/Controllers/Roblox.PrivateMessages/v1/announcements.ts
+++ Roblox/Controllers/Roblox.PrivateMessages/v1/announcements.ts
@@ -29,21 +29,8 @@ export default {
 	method: 'all',
 	func: async (_req, res) => {
 		res.send({
-			collection: [
-				{
-					id: 1,
-					sender: {
-						id: 1,
-						name: 'NIGGERS',
-						displayName: '10231 3',
-					},
-					subject: 'THE NIGGERS ARE TAKING OVER ROBLOX!',
-					body: 'FUCK YOU',
-					created: '2021-02-19T03:58:50.202Z',
-					updated: '2021-02-19T03:58:50.202Z',
-				},
-			],
-			totalCollectionSize: 1,
+			collection: [],
+			totalCollectionSize: 0,
 		});
 	},
 };
