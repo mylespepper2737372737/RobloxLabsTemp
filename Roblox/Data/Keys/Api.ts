a8d0c887b59090506aa411483e1b221867342811
ROBLOX Team Foundation <github@roblox.com>
2021-03-16 21:46:17 +0000

    Add the PointsApiKey

--- Roblox/Data/Keys/Api.ts
+++ Roblox/Data/Keys/Api.ts
@@ -2,4 +2,5 @@ export namespace ApiKeys {
 	export const AbTesingApi = 'B030971C-2A32-41B3-B808-18BCBE3B3483';
 	export const GamePersistenceApi = '223101C8-6699-4F88-B1BF-E5381531ED27';
 	export const TestApi = '8DAE2E89-BCFA-4735-AB79-D9A07ABA9263';
+	export const PointsApi = 'E3C6F569-496C-47F4-A76E-4F699DF453C4';
 }
