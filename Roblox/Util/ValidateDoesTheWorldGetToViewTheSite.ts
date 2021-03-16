d9a309dd48ec7fec897e04760d2a143cf8e6a477
ROBLOX Team Foundation <github@roblox.com>
2021-03-16 22:21:41 +0000

    Update this

--- Roblox/Util/ValidateDoesTheWorldGetToViewTheSite.ts
+++ Roblox/Util/ValidateDoesTheWorldGetToViewTheSite.ts
@@ -9,6 +9,7 @@ import {
 
 DYNAMIC_FASTFLAGVARIABLE('DoesTheWorldGetToViewTheSite', false);
 DYNAMIC_FASTFLAGVARIABLE('CanAdminsBypassTheSystem', false);
+DYNAMIC_FASTFLAGVARIABLE('NoMaintenance', false);
 DYNAMIC_FASTSTRINGVARIABLE('RobloxLabsSecurityToken', 'ljWby+/HVsZXJLfRkoljWby+/HVsZXJLfRko9mPQ9mPQ==');
 
 export function ValidateDoesTheWorldGetToViewTheSite(
@@ -17,6 +18,7 @@ export function ValidateDoesTheWorldGetToViewTheSite(
 	secToken: string,
 	response: Response,
 	doNotRedirect = false,
+	allowNoMaintenance = false,
 ) {
 	if (method === 'OPTIONS') return true;
 	if (
@@ -24,7 +26,13 @@ export function ValidateDoesTheWorldGetToViewTheSite(
 		(secToken === DFString('RobloxLabsSecurityToken') || secToken === encodeURIComponent(DFString('RobloxLabsSecurityToken')))
 	)
 		return true;
+	if (!DFFlag('NoMaintenance') && DFFlag('DoesTheWorldGetToViewTheSite')) return true;
+
 	if (!DFFlag('DoesTheWorldGetToViewTheSite')) {
+		if (DFFlag('NoMaintenance') && allowNoMaintenance) {
+			response.status(503).send('The service is unavailable.');
+			return false;
+		}
 		if (!doNotRedirect) response.redirect(BaseURL.GetSecureBaseURL() + '/login/maintenance/?ReturnUrl=' + returnUrl);
 		return false;
 	}
