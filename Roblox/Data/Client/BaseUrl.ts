16708fec043b759bc6cbd2c8db2502a8c09a6d2b
ROBLOX Team Foundation <github@roblox.com>
2021-03-16 21:45:58 +0000

    Add a new method that contructs a service url with the given parameters

--- Roblox/Data/Client/BaseUrl.ts
+++ Roblox/Data/Client/BaseUrl.ts
@@ -7,4 +7,9 @@ export namespace BaseURL {
 	export function GetSecureBaseURL() {
 		return 'https://' + Roblox.Api.Constants.URLS.ROBLOX_WWW;
 	}
+	export function ConstructServicePath(subDomain: string, servicePath: string = '/', secure: boolean = true): string {
+		return `${(secure ? GetSecureBaseURL() : GetBaseURL()).replace(/www/, subDomain)}${
+			!servicePath.startsWith('/') ? `/${servicePath}` : servicePath
+		}`;
+	}
 }
