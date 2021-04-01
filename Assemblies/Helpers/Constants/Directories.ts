3df3e2f2123b112f470b06ed246db8919c32b7b7
ROBLOX Team Foundation <github@roblox.com>
2021-04-01 12:11:16 +0100

    Remove this, read the README for more information

--- Assemblies/Helpers/Constants/Directories.ts
+++
@@ -1,4 +0,0 @@
-// THIS FILE WAS AUTOMATICALLY GENERATED, DO NOT EDIT
-// !DISABLE-AUTO-SELECT-DIR
-export const __baseDirName = 'C:\\buildAgent\\work\\0800fc577294c34f';
-export const __sslDirName = __baseDirName + '\\SSL';
