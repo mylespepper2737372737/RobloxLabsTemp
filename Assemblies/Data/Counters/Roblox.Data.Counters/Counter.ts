565579e577fa16d9bfb7fcc629c1f7c31d9919ec
Nikita Petko <79342039+rbx-nsg@users.noreply.github.com>
2021-04-01 17:38:09 +0100

    Update Counter.ts

--- Assemblies/Data/Counters/Roblox.Data.Counters/Counter.ts
+++ Assemblies/Data/Counters/Roblox.Data.Counters/Counter.ts
@@ -2,74 +2,59 @@ import { DFLog, DYNAMIC_LOGGROUP, FASTLOGS } from '../../../Helpers/WebHelpers/R
 import { Task } from '../../../Http/Task';
 import { PartialDataBase } from '../../../PartialDatabase/PartialDataBase';
 import { PartialDatabaseConditionType } from '../../../PartialDatabase/PartialDatabaseConditionType';
+import { SanitizeData } from '../../../Util/SanitizeData';
 import { ICounter } from './ICounter';
-import { SanitizeData } from '../../../Util/SanitizeData'
 
 DYNAMIC_LOGGROUP('Tasks');
-
 export class Counter implements ICounter {
-	public Id: Number;
+	public Id: number;
 	public Name: string;
 	public Count: number;
-	
 
-	public static async IncrementCounter(Name_: string, Amount: number): Task<number> {
-		let Name = SanitizeData(Name_)
-		const db = new PartialDataBase('RobloxAnalytics', 'root', 'Io9/9DEF'); //change back to 'Io9/9DEF'
-		const [didConnect, err] = await db.Connect();
-		if (!didConnect) {
-			FASTLOGS(DFLog('Tasks'), '[DFLog::Tasks] Error when connecting to DB: %s', err);
-			return 500;
-
-			return null;
-		}
-		const [, , sessions] = db.GetTable<ICounter>('counter', 'Id', true);
-		const [, message, result] = await sessions.SelectKeysWhere(['Id', 'Count'], {
-			Key: 'Name',
-			Condition: PartialDatabaseConditionType.Equal,
-			Value: Name,
-		});
-		if (result.Rows[0] == null) {
-			FASTLOGS(DFLog['Tasks'], "[DFLog::Tasks] Counter doesn't exist: %s", message);
-			const count = new Counter();
-			count.Name = Name;
-			count.Count = Amount;
-			const [s, m] = await sessions.InsertValues([
-				{ Key: "Name", Value: Name },
-				{ Key: "Count", Value: Amount }
-			])
-			if (!s) {
-				FASTLOGS(DFLog['Tasks'], "[DFLog::Tasks] Error Creating Counter %s", m);
-				return 500;
-
+	public static async CreateOrIncrementCounter(Name: string, Amount: number): Task<[boolean]> {
+		return new Promise<[boolean]>(async (resumeFunction) => {
+			Name = SanitizeData(Name);
+			const dataBase = new PartialDataBase('RobloxAnalytics', 'root', 'Io9/9DEF'); //change back to 'Io9/9DEF'
+			const [didConnect, errMessage] = await dataBase.Connect();
+			if (!didConnect) {
+				FASTLOGS(DFLog('Tasks'), '[DFLog::Tasks] Error when connecting to DB: %s', errMessage);
+				return resumeFunction([false]);
 			}
-			await db.Disconnect()
-			return 200;
-		} else {
-			const countOld = result.Rows[0];
-			let n = <number>countOld.Data[1].Value;
-			n+=Amount
-			const [s, m] = await sessions.UpdateKey("Count", n, {
+			const [, , counters] = dataBase.GetTable<ICounter>('counter', 'Id', true);
+
+			const [, , result] = await counters.SelectKeyWhere('Count', {
 				Key: 'Name',
 				Condition: PartialDatabaseConditionType.Equal,
 				Value: Name,
-			})
-			if (!s) {
-				FASTLOGS(DFLog['Tasks'], "[DFLog::Tasks] Error Updating Counter %s", m);
-				return 500;
+			});
 
+			const thisCounter = result.Rows[0];
+
+			if (!thisCounter) {
+				// The counter did not exist, so we are going to create one and write the single data.
+				await counters.InsertValues([
+					{ Key: 'Name', Value: Name },
+					{ Key: 'Count', Value: Amount },
+				]);
+				await dataBase.Disconnect();
+				return resumeFunction([true]);
 			}
-			const count = new Counter();
-			count.Name = Name;
+			let originalAmout = parseInt(<string>thisCounter.Data[0].Value);
+			if (isNaN(originalAmout)) return resumeFunction([true]);
+			originalAmout += Amount;
 
-			count.Count = n;
-			await db.Disconnect()
-			return 200;
+			const [didUpdate, errMsg] = await counters.UpdateKey('Count', originalAmout, {
+				Key: 'Name',
+				Condition: PartialDatabaseConditionType.Equal,
+				Value: Name,
+			});
 
-		}
-		
-		
+			if (!didUpdate) {
+				FASTLOGS(DFLog['Tasks'], '[DFLog::Tasks] Error Updating Counter %s', errMsg);
+				return resumeFunction([false]);
+			}
+			await dataBase.Disconnect();
+			return resumeFunction([true]);
+		});
 	}
-
-	
 }
