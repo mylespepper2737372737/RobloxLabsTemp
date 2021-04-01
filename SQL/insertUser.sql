838545199eb0ae9ad905015bd7976ef1757e5124
ROBLOX Team Foundation <github@roblox.com>
2021-04-01 12:09:21 +0100

    Add some new SQL scripts

---
+++ SQL/insertUser.sql
@@ -0,0 +1,34 @@
+INSERT INTO 
+`robloxmembership`.`user` 
+(
+	`Id`,
+	`Name`,
+	`DisplayName`,
+	`MembershipType`,
+	`Description`,
+	`Created`,
+	`Updated`,
+	`IsBanned`,
+    `AgeBracket`,
+    `Roles`,
+    `CountryCode`,
+    `UserAbove13`,
+    `IsAdmin`, 
+    `AccountId`
+) VALUES 
+(
+	'USER_ID',
+    'USER_NAME',
+    'DISPLAY_NAME',
+    'MEMBERSHIP_TYPE',
+    'DESCRIPTION',
+    'CREATED_DATE',
+    'UPDATED_DATE',
+    'IS_BANNED',
+    'AGE_BRACKET',
+    'ROLES',
+    'LOCALE_ID',
+    'USER_ABOVE_13',
+    'IS_ADMIN',
+    'ACCOUNT_ID'
+);
