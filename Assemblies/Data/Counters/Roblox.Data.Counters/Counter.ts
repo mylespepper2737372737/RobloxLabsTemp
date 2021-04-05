6f5963d24f9979492b057e3b094de13f59ed560a
ROBLOX Team Foundation <github@roblox.com>
2021-04-05 12:00:46 +0100

    Update the counter class with cleanups and single connection code

--- Assemblies/Data/Counters/Roblox.Data.Counters/Counter.ts
+++ Assemblies/Data/Counters/Roblox.Data.Counters/Counter.ts
@@ -11,50 +11,65 @@ export class Counter implements ICounter {
 	public Name: string;
 	public Count: number;
 
-	public static async CreateOrIncrementCounter(Name: string, Amount: number): Task<[boolean]> {
-		return new Promise<[boolean]>(async (resumeFunction) => {
-			Name = SanitizeData(Name);
-			const dataBase = new PartialDataBase('RobloxAnalytics', 'root', 'Io9/9DEF'); //change back to 'Io9/9DEF'
-			const [didConnect, errMessage] = await dataBase.Connect();
-			if (!didConnect) {
-				FASTLOGS(DFLog('Tasks'), '[DFLog::Tasks] Error when connecting to DB: %s', errMessage);
-				return resumeFunction([false]);
+	private static isConnected: boolean;
+	private static connectionAttemptRunning: boolean;
+	private static dataBase: PartialDataBase;
+
+	private static async connectIfNotConnected(): Task<void> {
+		return new Promise<void>(async (resumeFunction) => {
+			if (!this.connectionAttemptRunning) {
+				this.dataBase = new PartialDataBase('RobloxAnalytics', 'root', 'Io9/9DEF');
+				this.connectionAttemptRunning = true;
+				const [didConnect, errMessage] = await this.dataBase.Connect();
+				if (!didConnect) {
+					FASTLOGS(DFLog('Tasks'), '[DFLog::Tasks] Error when connecting to DB: %s', errMessage);
+					return false;
+				}
+				this.isConnected = didConnect;
+				this.connectionAttemptRunning = false;
+			} else {
+				do {} while (this.connectionAttemptRunning);
+				resumeFunction();
 			}
-			const [, , counters] = dataBase.GetTable<ICounter>('counter', 'Id', true);
-
-			const [, , result] = await counters.SelectKeyWhere('Count', {
-				Key: 'Name',
-				Condition: PartialDatabaseConditionType.Equal,
-				Value: Name,
-			});
-
-			const thisCounter = result.Rows[0];
-
-			if (!thisCounter) {
-				// The counter did not exist, so we are going to create one and write the single data.
-				await counters.InsertValues([
-					{ Key: 'Name', Value: Name },
-					{ Key: 'Count', Value: Amount },
-				]);
-				await dataBase.Disconnect();
-				return resumeFunction([true]);
-			}
-			let originalAmout = parseInt(<string>thisCounter.Data[0].Value);
-			if (isNaN(originalAmout)) return resumeFunction([true]);
-			originalAmout += Amount;
-
-			const [didUpdate, errMsg] = await counters.UpdateKey('Count', originalAmout, {
-				Key: 'Name',
-				Condition: PartialDatabaseConditionType.Equal,
-				Value: Name,
-			});
-
-			if (!didUpdate) {
-				FASTLOGS(DFLog['Tasks'], '[DFLog::Tasks] Error Updating Counter %s', errMsg);
-				return resumeFunction([false]);
-			}
-			await dataBase.Disconnect();
-			return resumeFunction([true]);
 		});
+	}
+
+	public static async CreateOrIncrementCounter(Name: string, Amount: number): Task<boolean> {
+		Name = SanitizeData(Name);
+		if (!this.isConnected) await this.connectIfNotConnected();
+
+		const [, , counters] = this.dataBase.GetTable<ICounter>('counter', 'Id', true);
+
+		const [, , result] = await counters.SelectKeyWhere('Count', {
+			Key: 'Name',
+			Condition: PartialDatabaseConditionType.Equal,
+			Value: Name,
+		});
+
+		const thisCounter = result.Rows[0];
+
+		if (!thisCounter) {
+			// The counter did not exist, so we are going to create one and write the single data.
+			await counters.InsertValues([
+				{ Key: 'Name', Value: Name },
+				{ Key: 'Count', Value: Amount },
+			]);
+			return true;
+		}
+		let originalAmout = parseInt(<string>thisCounter.Data[0].Value);
+		if (isNaN(originalAmout)) return true;
+		originalAmout += Amount;
+
+		const [didUpdate, errMsg] = await counters.UpdateKey('Count', originalAmout, {
+			Key: 'Name',
+			Condition: PartialDatabaseConditionType.Equal,
+			Value: Name,
+		});
+
+		if (!didUpdate) {
+			FASTLOGS(DFLog['Tasks'], '[DFLog::Tasks] Error Updating Counter %s', errMsg);
+			return false;
+		}
+		return true;
 	}
 }
