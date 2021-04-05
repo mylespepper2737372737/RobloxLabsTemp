7c5c6e60e79c497e6c6c23d0d5fd0c16fccc223a
ROBLOX Team Foundation <github@roblox.com>
2021-04-05 12:12:53 +0100

    Update the IDataStoreRequest

--- Assemblies/Platform/GamePersistence/IDataStoreRequest.ts
+++ Assemblies/Platform/GamePersistence/IDataStoreRequest.ts
@@ -1,7 +1,8 @@
+import { IUser } from '../Membership/IUser';
 import { IUniverse } from '../Universes/IUniverse';
 
 export interface IDataStoreRequest {
 	/*IUniverse*/ Universe: IUniverse;
-	/*String*/ UserSecurityToken: String;
-	/*Int32*/ MaxItemsToReturn: Number;
+	/*IUser*/ User: IUser;
+	/*Int32*/ MaxItemsToReturn: System.Int32;
 }
