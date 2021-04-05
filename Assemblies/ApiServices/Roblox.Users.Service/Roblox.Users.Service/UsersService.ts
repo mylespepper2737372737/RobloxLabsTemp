febbf907d21b9d16f5600dfa17506767d588075f
ROBLOX Team Foundation <github@roblox.com>
2021-04-05 11:51:34 +0100

    Add a TEMP implementation for the username validation until DB is fully setup

--- Assemblies/ApiServices/Roblox.Users.Service/Roblox.Users.Service/UsersService.ts
+++ Assemblies/ApiServices/Roblox.Users.Service/Roblox.Users.Service/UsersService.ts
@@ -1,1 +1,23 @@
-export namespace UsersService {}
+import { Response } from 'express';
+import { IServiceUsernameValidationRequest } from '../../../ApiClients/Roblox.Users.Client/Models/IServiceUsernameValidationRequest';
+import { IServiceUsernameValidationResponse } from '../../../ApiClients/Roblox.Users.Client/Models/IServiceUsernameValidationResponse';
+import { UsernameValidationStatus } from '../../../ApiSites/Roblox.Auth.Api/Models/UsernameValidationStatus';
+
+export namespace UsersService {
+	export namespace Validators {
+		export function ValidateUsername(
+			request: IServiceUsernameValidationRequest,
+			response: Response<IServiceUsernameValidationResponse>,
+		) {
+			return response.send({
+				IUser: request.IUser,
+				Status: UsernameValidationStatus.ValidUsername,
+				Username: request.Request.username,
+				Context: request.Request.context,
+				IsRCC: false,
+				Birthdate: request.Request.birthday,
+				InternalMessage: 'Username is valid',
+			});
+		}
+	}
+}
