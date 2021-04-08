61fa6fdf2ca02e7408dc54cdb4c2cfcccae418ee
ROBLOX Team Foundation <github@roblox.com>
2021-04-08 16:16:25 +0100

    Update C-T validator

--- Assemblies/Web/Util/Roblox.Web.Util/Validators/ContentTypeValidator.ts
+++ Assemblies/Web/Util/Roblox.Web.Util/Validators/ContentTypeValidator.ts
@@ -3,16 +3,37 @@ import { ICustomError } from '../../../../Platform/ErrorModels/Roblox.Platform.E
 import { Errors } from '../Errors';
 
 export namespace ContentTypeValidator {
-	export function CheckContentType(originalContentType: string, contentTypeToValidate: string, response: Response) {
-		return CheckContentTypes(originalContentType, [contentTypeToValidate], response);
+	export function CheckContentType(
+		originalContentType: string,
+		contentTypeToValidate: string,
+		response: Response,
+		hasBody: boolean = false,
+		isService: boolean = false,
+	) {
+		return CheckContentTypes(originalContentType, [contentTypeToValidate], response, hasBody, isService);
 	}
-	export function CheckContentTypes(originalContentType: string, contentTypesToValidate: string[], response: Response): boolean {
+	export function CheckContentTypes(
+		originalContentType: string,
+		contentTypesToValidate: string[],
+		response: Response,
+		hasBody: boolean = false,
+		isService: boolean = false,
+	): boolean {
 		const errors: ICustomError[] = [];
 		if (!originalContentType) {
+			const errorMessage =
+				"The request contains an entity body but no Content-Type header. The inferred media type 'application/octet-stream' is not supported for this resource.";
+			if (isService) {
+				if (hasBody) {
+					Errors.RespondWithAServiceError(415, errorMessage, response, true);
+				} else {
+					Errors.RespondWithAServiceError(500, 'An error has occurred.', response, true);
+				}
+				return false;
+			}
 			errors.push({
 				code: 0,
-				message:
-					"The request contains an entity body but no Content-Type header. The inferred media type 'application/octet-stream' is not supported for this resource.",
+				message: errorMessage,
 			});
 			Errors.RespondWithCustomErrors(415, errors, response, true);
 			return false;
@@ -28,9 +49,15 @@ export namespace ContentTypeValidator {
 			return true;
 		});
 		if (!contentTypeIsValid) {
+			const errorMessage = `The request entity's media type '${originalContentType}' is not supported for this resource.`;
+			if (isService) {
+				Errors.RespondWithAServiceError(415, errorMessage, response, true);
+				return false;
+			}
+
 			errors.push({
 				code: 0,
-				message: `The request entity's media type '${originalContentType}' is not supported for this resource.`,
+				message: errorMessage,
 			});
 			Errors.RespondWithCustomErrors(415, errors, response, true);
 			return false;
