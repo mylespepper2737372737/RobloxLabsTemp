f954aed7303d8c067c061c78d14af3cf9868db25
ROBLOX Team Foundation <github@roblox.com>
2021-04-08 16:12:16 +0100

    Update the tests

--- Assemblies/Tests/UserTests.ts
+++ Assemblies/Tests/UserTests.ts
@@ -1,7 +1,7 @@
 import { User } from '../Platform/Membership/User';
 
 (async () => {
-	const user = await User.GetById(2);
+	const user = await User.Get(2);
 	console.log(user);
 	const userByCookie = await User.GetByCookie('TEST');
 	console.log(userByCookie);
--- Assemblies/Tests/UtilTests.ts
+++ Assemblies/Tests/UtilTests.ts
@@ -1,4 +1,9 @@
+import { CheckDoesStringIncludeWhitespace } from '../Util/CheckDoesStringIncludeWhitespace';
 import { ClassWalker } from '../Util/ClassWalker';
 
 console.log(ClassWalker(require('./Entities/T')));
 console.log('done');
+console.log(CheckDoesStringIncludeWhitespace('abc '));
+console.log(CheckDoesStringIncludeWhitespace('abc'));
+console.log(CheckDoesStringIncludeWhitespace('test\n'));
+console.log(CheckDoesStringIncludeWhitespace('test\n\n'));
