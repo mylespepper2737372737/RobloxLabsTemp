d869997a971c8f38b3926ad846a0b7d827a33b56
ROBLOX Team Foundation <github@roblox.com>
2021-04-08 16:07:58 +0100

    Edit entity names, and update the connection method, add GetByName, CheckIfIdExists and CheckIfUsernameExists

--- Assemblies/Platform/Membership/User.ts
+++ Assemblies/Platform/Membership/User.ts
@@ -74,7 +74,7 @@ export class User implements IUser {
 		}
 		const thisSession = result.Rows[0];
 		if (!thisSession) return null;
-		return await this.GetById(<number>thisSession.Data[0].Value);
+		return await this.Get(<number>thisSession.Data[0].Value);
 	}
 
 	/**
@@ -82,7 +82,7 @@ export class User implements IUser {
 	 * @param {number} Id The userId.
 	 * @returns {IUser} Returns an IUser.
 	 */
-	public static async GetById(Id: number): Task<IUser> {
+	public static async Get(Id: number): Task<IUser> {
 		if (!this.isConnected) await this.connectIfNotConnected();
 		const [, , users] = this.db.GetTable<IUser>('User', 'Id', true);
 		const [success, message, result] = await users.SelectKeysWhere(
@@ -126,5 +126,64 @@ export class User implements IUser {
 		user.UserAbove13 = <boolean>(thisUser.Data[9].Value === 1);
 		user.IsAdmin = <boolean>(thisUser.Data[10].Value === 1);
 		return user;
+	}
+
+	/**
+	 * Partial implementation, full implementation when DataBase is fully setup.
+	 * @param {string} Name The userName.
+	 * @returns {IUser} Returns an IUser.
+	 */
+	public static async GetByName(Name: string): Task<IUser> {
+		if (!this.isConnected) await this.connectIfNotConnected();
+		const [, , users] = this.db.GetTable<IUser>('User', 'Id', true);
+		const [success, message, result] = await users.SelectKeysWhere(
+			[
+				'Id',
+				'DisplayName',
+				'MembershipType',
+				'Description',
+				'Created',
+				'IsBanned',
+				'AgeBracket',
+				'Roles',
+				'CountryCode',
+				'UserAbove13',
+				'IsAdmin',
+			],
+			{
+				Key: 'Name',
+				Condition: PartialDatabaseConditionType.Equal,
+				Value: Name,
+			},
+		);
+		if (!success) {
+			FASTLOGS(DFLog('Tasks'), '[DFLog::Tasks] Error when fetching user: %s', message);
+			return null;
+		}
+		const thisUser = result.Rows[0];
+		if (!thisUser) return null;
+
+		const user = new User();
+		user.Id = <number>thisUser.Data[0].Value;
+		user.Name = Name;
+		user.DisplayName = <string>thisUser.Data[1].Value;
+		user.MembershipType = <UserModelBuildersClubMembershipTypeEnum>thisUser.Data[2].Value;
+		user.Description = <string>thisUser.Data[3].Value;
+		user.Created = <string>thisUser.Data[4].Value;
+		user.IsBanned = <boolean>(thisUser.Data[5].Value === 1);
+		user.AgeBracket = <number>thisUser.Data[6].Value;
+		user.Roles = <string[]>JSON.parse(<string>thisUser.Data[7].Value);
+		user.CountryCode = <string>thisUser.Data[8].Value;
+		user.UserAbove13 = <boolean>(thisUser.Data[9].Value === 1);
+		user.IsAdmin = <boolean>(thisUser.Data[10].Value === 1);
+		return user;
+	}
+
+	public static async CheckIfIdExists(Id: number): Task<boolean> {
+		return (await this.Get(Id)) !== null;
+	}
+
+	public static async CheckIfUsernameExists(Name: string): Task<boolean> {
+		return (await this.GetByName(Name)) !== null;
 	}
 }
