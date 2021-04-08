f7f4a3e3607fcbe6bac109bd6c10c19065e8fc0f
ROBLOX Team Foundation <github@roblox.com>
2021-04-08 15:57:17 +0100

    Update the placeInfo staticFile

--- InternalCDN/PlaceInfo.lua
+++ InternalCDN/PlaceInfo.lua
@@ -1,5 +1,5 @@
 -- Loaded by StartGameSharedScript --
-pcall(function() game:SetCreatorID(0, Enum.CreatorType.User) end)
+pcall(function() game:SetCreatorID(1, Enum.CreatorType.User) end)
 
 pcall(function() game:GetService("SocialService"):SetFriendUrl("http://assetgame.sitetest4.robloxlabs.com/Game/LuaWebService/HandleSocialRequest.ashx?method=IsFriendsWith&playerid=%d&userid=%d") end)
 pcall(function() game:GetService("SocialService"):SetBestFriendUrl("http://assetgame.sitetest4.robloxlabs.com/Game/LuaWebService/HandleSocialRequest.ashx?method=IsBestFriendsWith&playerid=%d&userid=%d") end)
@@ -7,7 +7,7 @@ pcall(function() game:GetService("SocialService"):SetGroupUrl("http://assetgame.
 pcall(function() game:GetService("SocialService"):SetGroupRankUrl("http://assetgame.sitetest4.robloxlabs.com/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRank&playerid=%d&groupid=%d") end)
 pcall(function() game:GetService("SocialService"):SetGroupRoleUrl("http://assetgame.sitetest4.robloxlabs.com/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRole&playerid=%d&groupid=%d") end)
 pcall(function() game:GetService("GamePassService"):SetPlayerHasPassUrl("http://api.sitetest4.robloxlabs.com/Game/GamePass/GamePassHandler.ashx?Action=HasPass&UserID=%d&PassID=%d") end)
-pcall(function() game:GetService("MarketplaceService"):SetProductInfoUrl("http://api.roblox.com/marketplace/productinfo?assetId=%d") end)
-pcall(function() game:GetService("MarketplaceService"):SetDevProductInfoUrl("http://api.roblox.com/marketplace/productDetails?productId=%d") end)
+pcall(function() game:GetService("MarketplaceService"):SetProductInfoUrl("http://api.sitetest4.robloxlabs.com/marketplace/productinfo?assetId=%d") end)
+pcall(function() game:GetService("MarketplaceService"):SetDevProductInfoUrl("http://api.sitetest4.robloxlabs.com/marketplace/productDetails?productId=%d") end)
 pcall(function() game:GetService("MarketplaceService"):SetPlayerOwnsAssetUrl("http://api.sitetest4.robloxlabs.com/ownership/hasasset?userId=%d&assetId=%d") end)
 pcall(function() game:SetPlaceVersion(0) end)
