e214037978f457504e0dbf88ea2f1e8903d93470
ROBLOX Team Foundation <github@roblox.com>
2021-04-09 15:45:46 +0100

    Add BatchSequenceIncrementations

--- Assemblies/ApiServices/Roblox.EphemeralCounters.Service/Roblox.EphemeralCounters.Service/EphemeralCountersService.ts
+++ Assemblies/ApiServices/Roblox.EphemeralCounters.Service/Roblox.EphemeralCounters.Service/EphemeralCountersService.ts
@@ -1,8 +1,17 @@
 import { Response } from 'express';
 import { Counter } from '../../../Data/Counters/Roblox.Data.Counters/Counter';
+import { Sequence } from '../../../Data/Counters/Roblox.Data.Counters/Sequence';
+import { ISequencesItem } from '../../../Services/Roblox.EphemeralCounters.Service/ISequencesItem';
 import { ICounter } from '../../../Services/Roblox.EphemeralCounters.Service/Models/ICounter';
 
 export namespace EphemeralCountersService {
+	export async function HandleBatchSequences(sequences: ISequencesItem[], response: Response) {
+		sequences.forEach(async (sequence) => {
+			await IncrementSequence(sequence.Name, sequence.Context, sequence.Action, sequence.Value);
+		});
+		response.status(200).send();
+	}
+
 	export async function HandleIncrementCounter(counter: string, amount: number, response: Response) {
 		if (!(await IncrementCounter(counter, amount))) {
 			// TODO Implement a ServiceError here.
@@ -24,5 +33,10 @@ export namespace EphemeralCountersService {
 	export async function IncrementCounter(counter: string, amount: number) {
 		const didIncrement = await Counter.CreateOrIncrementCounter(counter, amount);
 		return didIncrement;
+	}
+
+	export async function IncrementSequence(name: string, context: string, action: string, value: number) {
+		const didUpdate = await Sequence.IncrementOrCreate(context, name, action, value);
+		return didUpdate;
 	}
 }
