1bc4dd7831563485d8ffa532269ca1ae79a9d550
ROBLOX Team Foundation <github@roblox.com>
2021-04-10 11:56:21 +0100

    Update default directories file

--- Source/Assemblies/Common/Constants/Roblox.Common.Constants/Directories.ts
+++ Source/Assemblies/Common/Constants/Roblox.Common.Constants/Directories.ts
@@ -1,4 +1,2 @@
-// THIS FILE WAS AUTOMATICALLY GENERATED, DO NOT EDIT
-// !DISABLE-AUTO-SELECT-DIR
-export const __baseDirName = 'C:\\buildAgent\\work\\0800fc577294c34f';
+export const __baseDirName = '';
 export const __sslDirName = __baseDirName + '\\SSL';
