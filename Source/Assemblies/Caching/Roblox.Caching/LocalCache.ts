33bcc16c471fa63a1ef3b4d5adf85ed2d87deb36
ROBLOX Team Foundation <github@roblox.com>
2021-05-09 02:51:57 +0100

    Update LocalCache

--- Source/Assemblies/Caching/Roblox.Caching/LocalCache.ts
+++ Source/Assemblies/Caching/Roblox.Caching/LocalCache.ts
@@ -4,6 +4,9 @@ import { CacheInfo } from './CacheInfo';
 import { ItemFetcher } from './ItemFetcher';
 
 export class LocalCache implements IDisposable {
+	Dispose(): void {
+		throw new Error('Method not implemented.');
+	}
 	public static GetEntityFromCache<TIndex, TEntity>(cacheInfo: CacheInfo, entityId: TIndex, getter: () => any): TEntity {
 		this.FillItemFetcher(getter, new ItemFetcher());
 		return;
