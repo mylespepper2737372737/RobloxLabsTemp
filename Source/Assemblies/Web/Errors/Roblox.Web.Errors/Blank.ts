92a1199400940ab0b6d95b95825eb156c7f11521
Nikita Petko <petko@mfdlabs.com>
2021-06-14 12:56:22 +0100

    Turn BlankErrorHandler into a Factory (#6) ~ Turn the BlankErrorHander into a factory in order to tell it if it can respond with a status 404 on not found (200 by default)

--- Source/Assemblies/Web/Errors/Roblox.Web.Errors/Blank.ts
+++ Source/Assemblies/Web/Errors/Roblox.Web.Errors/Blank.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 
-export const Blank = (_req: Request, res: Response) => {
-	res.status(200).send();
-};
+export const Blank =
+	(use404: boolean = false) =>
+	(_req: Request, res: Response) => {
+		res.status(use404 ? 404 : 200).send();
+	};
