57ed7bd9dfa1f6650bf9bab09911268b3d72cc2f
Jack Daniels <jrigger@mfdlabs.com>
2021-06-14 13:03:08 +0100

    Add the OriginMasterDescriptive for Hosts (#11) ~ Add the OriginMasterDescriptive just to specify hosts when verifying the access control origin
    Co-Authored-by: Nikita Petko <petko@mfdlabs.com>

--- Source/Assemblies/Web/Util/Roblox.Web.Util/OriginMaster.ts
+++ Source/Assemblies/Web/Util/Roblox.Web.Util/OriginMaster.ts
@@ -14,3 +14,16 @@ export class OriginMaster {
 		}
 	}
 }
+
+export class OriginMasterDescriptive {
+	public static Do(appHost: string, origin: string, protocol: string, responseBase: Response) {
+		const host = new URL(origin || `http://no-origin.com`);
+
+		if (host.hostname in FileBaseUrls) {
+			responseBase.setHeader('Access-Control-Allow-Origin', StripTheTrailingSlash(host.toString()));
+			responseBase.setHeader('Access-Control-Allow-Credentials', 'true');
+		} else {
+			if (!origin) responseBase.setHeader('Access-Control-Allow-Origin', `${protocol}://${appHost}`);
+		}
+	}
+}
