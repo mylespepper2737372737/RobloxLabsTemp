2975a03bfc7c739231228d3b3750626cc58c1969
myles20214 <92761149+myles20214@users.noreply.github.com>
2021-12-14 09:07:09 -0500

    Update UpsellAdModal.js

--- StaticPages/Websites/Roblox.Website/js/UpsellAdModal.js
+++ StaticPages/Websites/Roblox.Website/js/UpsellAdModal.js
@@ -1,35 +1,37 @@
-﻿Roblox = Roblox || {};
+Roblox = Roblox || {};
 
-if (typeof Roblox.UpsellAdModal === 'undefined') {
-	Roblox.UpsellAdModal = (function () {
-		var open = function () {
-			var options = {
-				titleText: Roblox.UpsellAdModal.Resources.title,
-				bodyContent: Roblox.UpsellAdModal.Resources.body,
-				footerText: '',
-				overlayClose: true,
-				escClose: true,
-				acceptText: Roblox.UpsellAdModal.Resources.accept,
-				declineText: Roblox.UpsellAdModal.Resources.decline,
-				acceptColor: Roblox.GenericConfirmation.green,
-				onAccept: function () {
-					window.location.href = '/premium/membership';
-				},
-				imageUrl: '/images/BuildersClub-110x110_small.png',
-			};
+if (typeof Roblox.UpsellAdModal === "undefined") {
+    Roblox.UpsellAdModal = function() {
+        var open = function() {
+            var options = {
+                titleText: Roblox.UpsellAdModal.Resources.title,
+                bodyContent: Roblox.UpsellAdModal.Resources.body,
+                footerText: "",
+                overlayClose: true,
+                escClose: true,
+                acceptText: Roblox.UpsellAdModal.Resources.accept,
+                declineText: Roblox.UpsellAdModal.Resources.decline,
+                acceptColor: Roblox.GenericConfirmation.green,
+                onAccept: function() { window.location.href = '/Upgrades/BuildersClubMemberships.aspx'; },
+                imageUrl: '/images/BuildersClub-110x110_small.png'
+            };
 
-			Roblox.GenericConfirmation.open(options);
-		};
+            Roblox.GenericConfirmation.open(
+                options
+            );
+        };
 
-		return {
-			open: open,
-		};
-	})();
+        return {
+            open: open
+        };
+    } ();
 }
 
 $(function () {
-	$('a.UpsellAdButton').click(function () {
-		Roblox.UpsellAdModal.open();
-		return false;
-	});
+    $('a.UpsellAdButton').click(function () {
+        Roblox.UpsellAdModal.open();
+        return false;
+    });
 });
+
+}
